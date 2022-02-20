8cd186788fe91ee42d1865546586c71900d1e2c2
Михаил <hugehug@mail.ru>
2022-02-20 17:29:14 +0700

    transfer functions from ToString to constructor

--- RomanNumbers/RomanNumber.cs
+++ RomanNumbers/RomanNumber.cs
@@ -1,10 +1,84 @@
 ﻿internal class RomanNumber : ICloneable, IComparable
 {
     private ushort number;
+    private string romanNumber;
     internal RomanNumber(ushort n)
     {
         if (n > 0)
+        {
             number = n;
+            romanNumber = "";
+
+            Dictionary<ushort, string> rDigits = new Dictionary<ushort, string>()
+            {
+                {1000, "M"},
+                {500, "D"},
+                {100, "C"},
+                {50, "L"},
+                {10, "X"},
+                {5, "V"},
+                {1, "I"}
+            };
+
+            foreach (var digit in rDigits)
+            {
+                if (digit.Key == number)
+                {
+                    romanNumber = digit.Value;
+                    return;
+                }
+            }
+
+            Dictionary<short, string> rRDigits = new Dictionary<short, string>()
+            {
+                {-1, "I"},
+                {-5, "V"},
+                {-10, "X"},
+                {-50, "L"},
+                {-100, "C"},
+                {-500, "D"},
+                {-1000, "M"},
+            };
+
+            int buff = number;
+
+            short combineDigits(ushort highDigit)
+            {
+                foreach (var digit in rRDigits)
+                {
+                    if (digit.Key + highDigit != 0 && buff / (digit.Key + highDigit) > 0)
+                        return digit.Key;
+                }
+                return 0;
+            }
+
+            void writeSymbols(int n, string symb)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    romanNumber += symb;
+                }
+            }
+
+            foreach (var digit in rDigits)
+            {
+                int count = buff / digit.Key;
+                if (count > 0 && count < 4)
+                {
+                    writeSymbols(count, digit.Value);
+                    buff -= digit.Key * count;
+                }
+                else
+                {
+                    short revDigit = combineDigits(digit.Key);
+                    if (revDigit != 0)
+                    {
+                        romanNumber += rRDigits[revDigit] + digit.Value;
+                        buff -= digit.Key + revDigit;
+                    }
+                }
+            }
+        }
         else
             throw new RomanNumberException("Некорректное число");
     }
@@ -50,75 +124,9 @@
     }
     public override string ToString()
     {
-        Dictionary<ushort, string> rDigits = new Dictionary<ushort, string>()
-        {
-            {1000, "M"},
-            {500, "D"},
-            {100, "C"},
-            {50, "L"},
-            {10, "X"},
-            {5, "V"},
-            {1, "I"}
-        };
-
-        foreach (var digit in rDigits)
-        {
-            if(digit.Key == number)
-                return digit.Value;
-        }
-
-            Dictionary<short, string> rRDigits = new Dictionary<short, string>()
-        {
-            {-1, "I"},
-            {-5, "V"},
-            {-10, "X"},
-            {-50, "L"},
-            {-100, "C"},
-            {-500, "D"},
-            {-1000, "M"},
-        };
-
-        string romanNumber = "";
-        int buff = number;
-
-        short combineDigits(ushort highDigit)
-        {
-            foreach (var digit in rRDigits)
-            {
-                if (digit.Key + highDigit != 0 && buff / (digit.Key + highDigit) > 0)
-                    return digit.Key;
-            }
-            return 0;
-        }
-
-        void writeSymbols(int n, string symb)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                romanNumber += symb;
-            }
-        }
-
-        foreach (var digit in rDigits)
-        {
-            int count = buff / digit.Key;
-            if (count > 0 && count < 4)
-            {
-                writeSymbols(count, digit.Value);
-                buff -= digit.Key * count;
-            }
-            else
-            {
-                short revDigit = combineDigits(digit.Key);
-                if (revDigit != 0)
-                {
-                    romanNumber += rRDigits[revDigit] + digit.Value;
-                    buff -= digit.Key + revDigit;
-                }
-            }
-        }
-
-        return romanNumber;
-        throw new NotImplementedException();
+        if(romanNumber != "")
+            return romanNumber;
+        else
+            throw new RomanNumberException("Число не найдено");
     }
 }
